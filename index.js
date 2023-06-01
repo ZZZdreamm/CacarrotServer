e7d501cfeedbd0a34056bca6dea8ac3582a91e26
ZZZdream <kmultan1234@gmail.com>
2023-06-01 12:34:05 +0200

    s

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require('cors')
 const server = express();
 
-const port = 5001
+const port = process.env.PORT || 5001
 server.use(express.static('public'))
 server.use(cors({origin:true}))
 server.use(express.json())
