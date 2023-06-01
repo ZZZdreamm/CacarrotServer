adad4d4f0c423f90f3606333cecd64359dc6cdc2
ZZZdream <kmultan1234@gmail.com>
2023-06-01 13:02:07 +0200

    sockets

--- index.js
+++ index.js
@@ -1,42 +1,57 @@
-
 const express = require("express");
-const cors = require('cors')
-const server = express();
+const cors = require("cors");
 
-const port = process.env.PORT || 5001
-server.use(express.static('public'))
-server.use(cors({origin:true}))
-server.use(express.json())
+const app = express();
+const http = require("http");
 
+app.use(express.static("public"));
+app.use(cors({ origin: true }));
+app.use(express.json());
 
-server.get("/", (request, response) => {
-
-
-    // Return the modified data
-    response.send({ result: "magiczka" });
-    // response.send("Hello world");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-server.get("/some-data", (request, response) => {
+const port = process.env.PORT || 5000;
 
 
-    // Return the modified data
-    response.send({ result: "karoca" });
-    // response.send("Hello world");
+io.on("connection", (socket) => {
+    const players = []
+  socket.on("data", (data) => {
+    players.push(data)
+    console.log(players);
+    io.emit("changedData", players);
+  });
+  console.log("a user connected");
 });
 
-server.post("/some-data", (request, response) => {
-    const {data} = request.body; // Assuming you're using a body-parser or similar middleware to parse the request body
-    console.log(data)
-
-    // // Modify the data
-    const modifiedData = data.toUpperCase();
-
-    // Return the modified data
-    response.send({ result: modifiedData });
-    // response.send("Hello world");
+app.get("/", (request, response) => {
+  // Return the modified data
+  response.send({ result: "magiczka" });
+  // response.send("Hello world");
 });
 
+app.get("/some-data", (request, response) => {
+  // Return the modified data
+  response.send({ result: "karoca" });
+  // response.send("Hello world");
+});
 
+app.post("/some-data", (request, response) => {
+  const { data } = request.body; // Assuming you're using a body-parser or similar middleware to parse the request body
+  console.log(data);
 
-server.listen(port, ()=> console.log('dzialam'))
+  // // Modify the data
+  const modifiedData = data.toUpperCase();
+
+  // Return the modified data
+  response.send({ result: modifiedData });
+  // response.send("Hello world");
+});
+
+server.listen(port, () => console.log("dzialam"));
